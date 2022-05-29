ac48eb96917692c18869d2ac519739c5601550d0
Cristiano Augusto <cristiano.asg@hotmail.com>
2022-05-29 14:38:39 +0100

    jsx structure

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>React Native</Text>
-      <Text>Ignite</Text>
-    </View >
+import { Home } from './src/pages/Home';
 
-  )
+export default function App(){
+  return <Home/>
 }
