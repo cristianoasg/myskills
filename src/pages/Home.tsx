f417ac6ce69142a9655d7df189e330d4672fe2fd
Cristiano Augusto <cristiano.asg@hotmail.com>
2022-06-27 21:34:22 +0100

    typing objects and states

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,23 @@ import {
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData {
+  id: String;
+  name: String;
+}
+
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
   const [gretting, setGretting] = useState('');
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills(oldState => [...oldState, data]);
   }
 
   useEffect(() => {
@@ -59,12 +69,11 @@ export function Home() {
       </Text>
 
 
-
       <FlatList
         data={mySkills}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={item} />
+          <SkillCard skill={item.name} />
         )}
       />
     </View >
